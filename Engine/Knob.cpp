MrKepzie/Natron

commit d27f94b523e634698022ff08cdf142814322c516

Engine: use consistent set/get method naming, and avoid misleading inversions (IsSignificant was the opposite of EvaluateOnChange)

Engine/Knob.cpp
@@ -47,11 +47,11 @@ struct Knob::KnobPrivate {
     int _itemSpacing;
     
     boost::shared_ptr<Knob> _parentKnob;
-    bool _secret;
+    bool _IsSecret;
     std::vector<bool> _enabled;
-    bool _canUndo;
-    bool _isInsignificant; //< if true, a value change will never trigger an evaluation
-    bool _isPersistent;//will it be serialized?
+    bool _CanUndo;
+    bool _EvaluateOnChange; //< if true, a value change will never trigger an evaluation
+    bool _IsPersistant;//will it be serialized?
     std::string _tooltipHint;
     bool _isAnimationEnabled;
     
@@ -79,11 +79,11 @@ struct Knob::KnobPrivate {
     , _newLine(true)
     , _itemSpacing(0)
     , _parentKnob()
-    , _secret(false)
+    , _IsSecret(false)
     , _enabled(dimension)
-    , _canUndo(true)
-    , _isInsignificant(false)
-    , _isPersistent(true)
+    , _CanUndo(true)
+    , _EvaluateOnChange(false)
+    , _IsPersistant(true)
     , _tooltipHint()
     , _isAnimationEnabled(true)
     , _values(dimension)
@@ -359,7 +359,7 @@ int Knob::getDimension() const { return _imp->_dimension; }
 void Knob::load(const KnobSerialization& serializationObj){
     
     assert(_imp->_dimension == serializationObj.getDimension());
-    assert(isPersistent()); // a non-persistent Knob should never be loaded!
+    assert(getIsPersistant()); // a non-persistent Knob should never be loaded!
     
     ///restore masters
     const std::vector< std::pair<int,std::string> >& serializedMasters = serializationObj.getMasters();
@@ -444,7 +444,7 @@ void Knob::load(const KnobSerialization& serializationObj){
 
 void Knob::save(KnobSerialization* serializationObj) const
 {
-    assert(isPersistent()); // a non-persistent Knob should never be saved!
+    assert(getIsPersistant()); // a non-persistent Knob should never be saved!
     QMutexLocker l(&_imp->_valueMutex);
     serializationObj->initialize(this);
 }
@@ -500,7 +500,7 @@ void Knob::endValueChange() {
 
 
 void Knob::evaluateValueChange(int dimension,Natron::ValueChangedReason reason){
-    if (!_imp->_isInsignificant) {
+    if (!_imp->_EvaluateOnChange) {
         _imp->_holder->invalidateHash();
     }
     processNewValue();
@@ -509,7 +509,7 @@ void Knob::evaluateValueChange(int dimension,Natron::ValueChangedReason reason){
     }
     emit updateSlaves(dimension);
     
-    bool significant = reason == Natron::TIME_CHANGED ? false : !_imp->_isInsignificant;
+    bool significant = reason == Natron::TIME_CHANGED ? false : !_imp->_EvaluateOnChange;
     if(!_imp->_holder->isClone()){
         _imp->_holder->notifyProjectEvaluationRequested(reason, this, significant);
     }
@@ -589,7 +589,7 @@ void Knob::setAllDimensionsEnabled(bool b) {
 }
 
 void Knob::setSecret(bool b){
-    _imp->_secret = b;
+    _imp->_IsSecret = b;
     emit secretChanged();
 }
 
@@ -618,7 +618,7 @@ bool Knob::hasAnimation() const {
 
 KnobHolder*  Knob::getHolder() const { return _imp->_holder; }
 
-void Knob::turnOffAnimation() { _imp->_isAnimationEnabled = false; }
+void Knob::setAnimationEnabled(bool val) { _imp->_isAnimationEnabled = val; }
 
 bool Knob::isAnimationEnabled() const { return canAnimate() && _imp->_isAnimationEnabled; }
 
@@ -630,21 +630,21 @@ void Knob::setParentKnob(boost::shared_ptr<Knob> knob){ _imp->_parentKnob = knob
 
 boost::shared_ptr<Knob> Knob::getParentKnob() const {return _imp->_parentKnob;}
 
-bool Knob::isSecret() const {return _imp->_secret;}
+bool Knob::getIsSecret() const {return _imp->_IsSecret;}
 
 bool Knob::isEnabled(int dimension) const { assert(dimension < getDimension()); return _imp->_enabled[dimension];}
 
-void Knob::setInsignificant(bool b) {_imp->_isInsignificant = b;}
+void Knob::setEvaluateOnChange(bool b) {_imp->_EvaluateOnChange = b;}
 
-bool Knob::isPersistent() const { return _imp->_isPersistent; }
+bool Knob::getIsPersistant() const { return _imp->_IsPersistant; }
 
-void Knob::setPersistent(bool b) { _imp->_isPersistent = b; }
+void Knob::setIsPersistant(bool b) { _imp->_IsPersistant = b; }
 
-void Knob::turnOffUndoRedo() {_imp->_canUndo = false;}
+void Knob::setCanUndo(bool val) {_imp->_CanUndo = val;}
 
-bool Knob::canBeUndone() const {return _imp->_canUndo;}
+bool Knob::getCanUndo() const {return _imp->_CanUndo;}
 
-bool Knob::isInsignificant() const {return _imp->_isInsignificant;}
+bool Knob::getEvaluateOnChange() const {return _imp->_EvaluateOnChange;}
 
 void Knob::setHintToolTip(const std::string& hint) {
     _imp->_tooltipHint = hint;

Engine/Knob.h
@@ -220,7 +220,7 @@ public:
 
     const std::vector< boost::shared_ptr<Curve>  >& getCurves() const;
 
-    void turnOffAnimation() ;
+    void setAnimationEnabled(bool val) ;
 
     /**
      * @brief Returns true if the animation is enabled for this knob. A return value of
@@ -268,21 +268,21 @@ public:
     
     int determineHierarchySize() const;
     
-    bool isSecret() const ;
+    bool getIsSecret() const ;
     
     bool isEnabled(int dimension) const;
     
-    void setInsignificant(bool b);
+    void setEvaluateOnChange(bool b);
     
-    bool isPersistent() const;
+    bool getIsPersistant() const;
 
-    void setPersistent(bool b);
+    void setIsPersistant(bool b);
 
-    void turnOffUndoRedo();
+    void setCanUndo(bool val);
     
-    bool canBeUndone() const;
+    bool getCanUndo() const;
     
-    bool isInsignificant() const;
+    bool getEvaluateOnChange() const;
     
     void setHintToolTip(const std::string& hint);
     

Engine/KnobTypes.cpp
@@ -496,7 +496,7 @@ Button_Knob::Button_Knob(KnobHolder  *holder, const std::string &description, in
 Knob(holder, description, dimension)
 , _renderButton(false)
 {
-    setPersistent(false);
+    setIsPersistant(false);
 }
 
 bool Button_Knob::canAnimate() const

Engine/KnobTypes.h
@@ -450,7 +450,7 @@ public:
     bool isMultiLine() const { return _multiLine; }
     
     void setAsLabel() {
-        turnOffAnimation(); //< labels cannot animate
+        setAnimationEnabled(false); //< labels cannot animate
         _isLabel = true;
     }
 

Engine/Node.cpp
@@ -199,7 +199,7 @@ void Node::loadKnobs(const NodeSerialization& serialization) {
         for (U32 k = 0; k < knobsValues.size(); ++k) {
             if(knobsValues[k]->getLabel() == nodeKnobs[j]->getDescription()){
                 // don't load the value if the Knob is not persistant! (it is just the default value in this case)
-                if (nodeKnobs[j]->isPersistent()) {
+                if (nodeKnobs[j]->getIsPersistant()) {
                     nodeKnobs[j]->load(*knobsValues[k]);
                 }
                 break;

Engine/NodeSerialization.cpp
@@ -22,7 +22,7 @@ void NodeSerialization::initialize(const Natron::Node* n){
     const std::vector< boost::shared_ptr<Knob> >& knobs = n->getKnobs();
     
     for (U32 i  = 0; i < knobs.size(); ++i) {
-        if(knobs[i]->isPersistent()){
+        if(knobs[i]->getIsPersistant()){
             boost::shared_ptr<KnobSerialization> newKnobSer(new KnobSerialization);
             knobs[i]->save(newKnobSer.get());
             _knobsValues.push_back(newKnobSer);

Engine/OfxImageEffectInstance.cpp
@@ -329,19 +329,14 @@ OFX::Host::Param::Instance *OfxImageEffectInstance::newParam(const std::string &
     }
     
     knob->setName(paramName);
-    knob->setInsignificant(!descriptor.getEvaluateOnChange());
-    knob->setPersistent(descriptor.getIsPersistant());
+    knob->setEvaluateOnChange(descriptor.getEvaluateOnChange());
+    knob->setIsPersistant(descriptor.getIsPersistant());
     //FIXME: replace the following with
-    //knob->setCanAnimate(descriptor.getCanAnimate());
-    if (!descriptor.getCanAnimate()) {
-#pragma message WARN("where is turnOnAnimation or setCanAnimate?")
-        knob->turnOffAnimation();
-    }
+    knob->setAnimationEnabled(descriptor.getCanAnimate());
     knob->setSecret(descriptor.getSecret());
     knob->setAllDimensionsEnabled(descriptor.getEnabled());
     knob->setHintToolTip(descriptor.getHint());
-    //FIXME: replace the following with
-    //knob->setCanUndo(descriptor.getCanUndo());
+    knob->setCanUndo(descriptor.getCanUndo());
     if (!descriptor.getCanUndo()) {
 #pragma message WARN("where is turnOnUndoRedo or setCanUndo?")
        knob->turnOffUndoRedo();

Engine/OfxParamInstance.cpp
@@ -160,7 +160,7 @@ void OfxPushButtonInstance::setSecret() {
 }
 
 void OfxPushButtonInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 boost::shared_ptr<Knob> OfxPushButtonInstance::getKnob() const {
@@ -217,7 +217,7 @@ void OfxIntegerInstance::setSecret() {
 }
 
 void OfxIntegerInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 boost::shared_ptr<Knob> OfxIntegerInstance::getKnob() const{
@@ -419,7 +419,7 @@ void OfxDoubleInstance::setSecret() {
 }
 
 void OfxDoubleInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 void OfxDoubleInstance::setDisplayRange(){
@@ -510,7 +510,7 @@ void OfxBooleanInstance::setSecret() {
 }
 
 void OfxBooleanInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 boost::shared_ptr<Knob> OfxBooleanInstance::getKnob() const{
@@ -598,7 +598,7 @@ void OfxChoiceInstance::setSecret() {
 }
 
 void OfxChoiceInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 
@@ -722,7 +722,7 @@ void OfxRGBAInstance::setSecret() {
 }
 
 void OfxRGBAInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 
@@ -833,7 +833,7 @@ void OfxRGBInstance::setSecret() {
 }
 
 void OfxRGBInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 
@@ -994,7 +994,7 @@ void OfxDouble2DInstance::setSecret() {
 }
 
 void OfxDouble2DInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 void OfxDouble2DInstance::setDisplayRange() {
@@ -1125,7 +1125,7 @@ void OfxInteger2DInstance::setSecret() {
 }
 
 void OfxInteger2DInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 
@@ -1258,7 +1258,7 @@ void OfxDouble3DInstance::setSecret() {
 }
 
 void OfxDouble3DInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 boost::shared_ptr<Knob> OfxDouble3DInstance::getKnob() const {
@@ -1373,7 +1373,7 @@ void OfxInteger3DInstance::setSecret() {
 }
 
 void OfxInteger3DInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 boost::shared_ptr<Knob> OfxInteger3DInstance::getKnob() const {
@@ -1470,7 +1470,7 @@ OfxStringInstance::OfxStringInstance(OfxEffectInstance* node,OFX::Host::Param::D
                 _fileKnob->setAsInputImage();
             }
             if (!filePathSupportsImageSequences) {
-                _fileKnob->turnOffAnimation();
+                _fileKnob->setAnimationEnabled(false);
             }
         } else {
             _outputFileKnob = Natron::createKnob<OutputFile_Knob>(node, getParamLabel(this));
@@ -1478,7 +1478,7 @@ OfxStringInstance::OfxStringInstance(OfxEffectInstance* node,OFX::Host::Param::D
                 _outputFileKnob->setAsOutputImageFile();
             }
             if (!filePathSupportsImageSequences) {
-                _outputFileKnob->turnOffAnimation();
+                _outputFileKnob->setAnimationEnabled(false);
             }
             
         }
@@ -1645,16 +1645,16 @@ void OfxStringInstance::setSecret(){
 
 void OfxStringInstance::setEvaluateOnChange() {
     if(_fileKnob){
-        _fileKnob->setInsignificant(!getEvaluateOnChange());
+        _fileKnob->setEvaluateOnChange(getEvaluateOnChange());
     }
     if (_outputFileKnob) {
-        _outputFileKnob->setInsignificant(!getEvaluateOnChange());
+        _outputFileKnob->setEvaluateOnChange(getEvaluateOnChange());
     }
     if (_stringKnob) {
-        _stringKnob->setInsignificant(!getEvaluateOnChange());
+        _stringKnob->setEvaluateOnChange(getEvaluateOnChange());
     }
     if (_pathKnob) {
-        _pathKnob->setInsignificant(!getEvaluateOnChange());
+        _pathKnob->setEvaluateOnChange(getEvaluateOnChange());
     }
 }
 
@@ -1826,7 +1826,7 @@ void OfxCustomInstance::setSecret() {
 }
 
 void OfxCustomInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 OfxStatus OfxCustomInstance::getNumKeys(unsigned int &nKeys) const {
@@ -1928,7 +1928,7 @@ void OfxParametricInstance::setSecret() {
 }
 
 void OfxParametricInstance::setEvaluateOnChange() {
-    _knob->setInsignificant(!getEvaluateOnChange());
+    _knob->setEvaluateOnChange(getEvaluateOnChange());
 }
 
 /// callback which should update label

commit 17f74bd25736ccc69de062251dfdbd0006ba2feb

Knob::evaluateValueChange: fix bug introduced in d27f94b523e634698022ff08cdf142814322c516 (easy to find now that there are no more inverse values, like isSignificant and isInsignificant)

Engine/Knob.cpp
@@ -520,18 +520,19 @@ void Knob::endValueChange() {
 }
 
 
-void Knob::evaluateValueChange(int dimension,Natron::ValueChangedReason reason){
-    if (!_imp->_EvaluateOnChange) {
+void Knob::evaluateValueChange(int dimension,Natron::ValueChangedReason reason)
+{
+    if (_imp->_EvaluateOnChange) {
         _imp->_holder->invalidateHash();
     }
     processNewValue();
-    if(reason != Natron::USER_EDITED && !_imp->_holder->isClone()){
+    if ((reason != Natron::USER_EDITED) && !_imp->_holder->isClone()) {
         emit valueChanged(dimension);
     }
     emit updateSlaves(dimension);
     
-    bool significant = reason == Natron::TIME_CHANGED ? false : !_imp->_EvaluateOnChange;
-    if(!_imp->_holder->isClone()){
+    bool significant = (reason != Natron::TIME_CHANGED) && _imp->_EvaluateOnChange;
+    if (!_imp->_holder->isClone()) {
         _imp->_holder->notifyProjectEvaluationRequested(reason, this, significant);
     }
 }